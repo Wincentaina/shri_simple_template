5bfdd670896c135a0de04a552005454da23c4d5e
Wincentaina <103582986+Wincentaina@users.noreply.github.com>
2022-10-28 20:27:10 +0700

    ll

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,7 @@ module.exports = {
         "browser": true,
         "es2021": true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
+    "extends": ["plugin:react/jsx-runtime"],
     "overrides": [
     ],
     "parserOptions": {
@@ -17,5 +14,6 @@ module.exports = {
         "react"
     ],
     "rules": {
+        "react/react-in-jsx-scope": "off"
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -4,96 +4,96 @@ const StatoscopePlugin = require('@statoscope/webpack-plugin').default;
 const webpack = require('webpack');
 
 const config = {
-    entry: {
-        about: './src/pages/About.js',
-        home: './src/pages/Home.js',
-        index: "./src/index.js"
-    },
+  entry: {
+    about: './src/pages/About.js',
+    home: './src/pages/Home.js',
+    index: './src/index.js',
+  },
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./public/index.html",
-        }),
-        new webpack.ProvidePlugin({
-            process: 'process/browser',
-        }),
-        new StatoscopePlugin({
-            saveStatsTo: 'stats.json',
-            saveReportTo: 'report.html',
-            saveOnlyStats: false,
-            open: false,
-        }),
-        new webpack.DefinePlugin({
-            'process.env': JSON.stringify(process.env)
-        })
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+    new StatoscopePlugin({
+      saveStatsTo: 'stats.json',
+      saveReportTo: 'report.html',
+      saveOnlyStats: false,
+      open: false,
+    }),
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(process.env),
+    }),
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: [/node_modules/],
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env', ['@babel/preset-react', { runtime: 'automatic' }]] },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+
     ],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[contenthash].js',
-        clean: true
+  },
+  optimization: {
+    usedExports: true,
+    concatenateModules: true,
+    minimize: true,
+    moduleIds: 'deterministic',
+    innerGraph: true,
+    splitChunks: {
+      minSize: 20000,
+      minRemainingSize: 0,
+      maxAsyncRequests: 25,
+      enforceSizeThreshold: 50000,
+      minChunks: 1,
+      chunks: 'all',
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node modules[\\]/,
+          priority: -10,
+          reuseExistingChunk: true,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
     },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: [/node_modules/],
-                loader: 'babel-loader',
-                options: { presets: ['@babel/env',["@babel/preset-react", {"runtime": "automatic"}]]},
-            },
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"]
-            },
+    runtimeChunk: {
+      name: 'runtime',
+    },
 
-        ],
+  },
+  target: 'web',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    fallback: {
+      buffer: require.resolve('buffer'),
+      stream: require.resolve('stream-browserify'),
+      crypto: require.resolve('crypto'),
     },
-    optimization: {
-        usedExports: true,
-        concatenateModules: true,
-        minimize: true,
-        moduleIds: "deterministic",
-        innerGraph: true,
-        splitChunks: {
-            minSize: 20000,
-            minRemainingSize: 0,
-            maxAsyncRequests: 25,
-            enforceSizeThreshold: 50000,
-            minChunks: 1,
-            chunks: "all",
-            cacheGroups: {
-                defaultVendors: {
-                    test: /[\\/]node modules[\\]/,
-                    priority: -10,
-                    reuseExistingChunk: true,
-                },
-                default: {
-                    minChunks: 2,
-                    priority: -20,
-                    reuseExistingChunk: true,
-                },
-            },
-        },
-        runtimeChunk: {
-            name: "runtime"
-        },
-
-    },
-    target: "web",
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"],
-        fallback: {
-            'buffer': require.resolve('buffer'),
-            'stream': require.resolve("stream-browserify"),
-            'crypto': require.resolve('crypto')
-        },
-        modules: [
-            path.resolve(__dirname, 'node_modules'),
-            path.resolve(__dirname, 'node_modules/ui/node_modules'),
-        ]
-    },
-    stats: {
-        children: true
-    }
+    modules: [
+      path.resolve(__dirname, 'node_modules'),
+      path.resolve(__dirname, 'node_modules/ui/node_modules'),
+    ],
+  },
+  stats: {
+    children: true,
+  },
 
 };
 
